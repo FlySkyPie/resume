4690a49417cd0a2ec5d129bc3be86d53f53176a9
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2026-02-10 18:31:59 +0800

    docs: Update readme

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig(({ command }) => {
   const base = command === 'serve' ?
     undefined :
-    '/client/';
+    '/resume/';
   return {
     plugins: [vike(), react()],
     build: {
